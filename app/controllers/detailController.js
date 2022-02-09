fac66554058f53e2c15bc2f9fee494664c31ba46
LoganW <logan.willaumez@gmail.com>
2022-02-09 15:57:41 +0100

    wait for responsive

--- app/controllers/detailController.js
+++ app/controllers/detailController.js
@@ -4,9 +4,20 @@ const detailController = {
   detailPokemon: async (req, res) => {
     const id = req.params.id;
     const pokemon = await dataMapper.getOnePokemon(id);
+    function normalize(stat) {
+      return (stat / 255) * 100;
+    }
+    const stats = {
+      attaque: normalize(pokemon.attaque),
+      defense: normalize(pokemon.defense),
+      attaque_spe: normalize(pokemon.attaque_spe),
+      defense_spe: normalize(pokemon.defense_spe),
+      vitesse: normalize(pokemon.vitesse),
+    };
     res.render("detail", {
       pokemon,
-      title: `detail of : ${pokemons.name}`,
+      stats,
+      title: `detail of : ${pokemon.name}`,
     });
   },
 };
