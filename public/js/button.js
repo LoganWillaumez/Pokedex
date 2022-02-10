5b6c1ced2dcd7ed18f8834e02cb66a650a1cd8b4
LoganW <logan.willaumez@gmail.com>
2022-02-10 21:55:30 +0100

    finalise

--- public/js/button.js
+++ public/js/button.js
@@ -1,10 +1,20 @@
 const btn = document.querySelector(".pokedex__filter");
 const filter = document.querySelector(".form__container");
 const closeBtn = document.querySelector(".btn");
+const pokedex = document.querySelector(".pokedex");
 
 btn.addEventListener("click", () => {
   filter.classList.toggle("filter__open");
 });
 closeBtn.addEventListener("click", () => {
   filter.classList.remove("filter__open");
+});
+document.addEventListener("click", (e) => {
+  console.log(e.target.className);
+  if (
+    e.target.className === "pokedex" ||
+    e.target.className === "pokedex__title"
+  ) {
+    filter.classList.remove("filter__open");
+  }
 });
